ca9db3a18e78553935fba79b092ddd0406991bf5
FatQuteDragon <FatQuteDragon@gmail.com>
2014-10-26 15:11:14 -0400

    Chage output message in GitTest. Also commit whatever system changes occurred last time after the initial commit.

--- gitTest/Adrian/firstGit/GitTest.java
+++ gitTest/Adrian/firstGit/GitTest.java
@@ -3,7 +3,7 @@ package Adrian.firstGit;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Git is fun!");
+		System.out.println("Git is totally super fun!");
 		
 	}
 
